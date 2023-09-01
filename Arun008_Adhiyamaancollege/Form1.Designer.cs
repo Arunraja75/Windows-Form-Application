561c24fad2a4534435fe0452ff0a0c6a7f882e2a
Arun R <arunraja711200@gmail.com>
2023-09-01 19:14:07 +0530

    Add files via upload

---
+++ Arun008_Adhiyamaancollege/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Arun008_Adhiyamaancollege
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_Registration = new System.Windows.Forms.Button();
+            this.txt_visitproducts = new System.Windows.Forms.Button();
+            this.txt_customercare = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 94);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(890, 467);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.label1.Font = new System.Drawing.Font("Imprint MT Shadow", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(45, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "A R U N\r\n";
+            // 
+            // txt_Registration
+            // 
+            this.txt_Registration.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.txt_Registration.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.txt_Registration.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Registration.ForeColor = System.Drawing.Color.White;
+            this.txt_Registration.Location = new System.Drawing.Point(235, 53);
+            this.txt_Registration.Name = "txt_Registration";
+            this.txt_Registration.Size = new System.Drawing.Size(152, 44);
+            this.txt_Registration.TabIndex = 1;
+            this.txt_Registration.Text = "REGISTRATION";
+            this.txt_Registration.UseVisualStyleBackColor = false;
+            this.txt_Registration.Click += new System.EventHandler(this.txt_Registration_Click);
+            // 
+            // txt_visitproducts
+            // 
+            this.txt_visitproducts.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.txt_visitproducts.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.txt_visitproducts.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_visitproducts.ForeColor = System.Drawing.Color.White;
+            this.txt_visitproducts.Location = new System.Drawing.Point(393, 53);
+            this.txt_visitproducts.Name = "txt_visitproducts";
+            this.txt_visitproducts.Size = new System.Drawing.Size(162, 44);
+            this.txt_visitproducts.TabIndex = 2;
+            this.txt_visitproducts.Text = "VISIT PRODUCTS";
+            this.txt_visitproducts.UseVisualStyleBackColor = false;
+            // 
+            // txt_customercare
+            // 
+            this.txt_customercare.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.txt_customercare.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.txt_customercare.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_customercare.ForeColor = System.Drawing.Color.White;
+            this.txt_customercare.Location = new System.Drawing.Point(561, 53);
+            this.txt_customercare.Name = "txt_customercare";
+            this.txt_customercare.Size = new System.Drawing.Size(151, 44);
+            this.txt_customercare.TabIndex = 3;
+            this.txt_customercare.Text = "CUSTOMER CARE";
+            this.txt_customercare.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(718, 53);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 44);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "ABOUT";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.PapayaWhip;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.txt_customercare);
+            this.panel1.Controls.Add(this.txt_visitproducts);
+            this.panel1.Controls.Add(this.txt_Registration);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(890, 96);
+            this.panel1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(890, 558);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button txt_Registration;
+        private System.Windows.Forms.Button txt_visitproducts;
+        private System.Windows.Forms.Button txt_customercare;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Arun008_Adhiyamaancollege/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Arun008_Adhiyamaancollege
+{
+    public partial class Form1 : Form
+    {
+        Registration rg = new Registration();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txt_Registration_Click(object sender, EventArgs e)
+        {
+            rg.show();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
